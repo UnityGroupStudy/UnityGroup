52f1d06709a522e97ac60d8adfac3efec1697f67
Liunx <kdh950812@gmail.com>
2019-10-04 13:40:53 +0900

    project init

---
+++ GroupGame/Assets/Resources/DongHyuk/Scripts/KeyController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    public static KeyController I = null;
+    public enum KeyType {
+        none, down, up, ll, dd
+    };
+
+    float limitTime = 1.0f;
+
+    public int keyLength;
+    public float[] keyTimes;
+    public float[] keyUpTimes;
+    public int[] keyValues;
+
+    public int[] keyDouble;
+    public int[] keyDoubleUp;
+    public int[] keyDoubleStay;
+
+    public int[] keyIndexs;
+
+    void Awake() {
+        if(I == null)
+            I = this;
+    }
+
+    void Start()
+    {
+        keyLength = (int)System.Enum.GetValues(typeof(KeyCode)).Length;
+
+        keyTimes = new float[keyLength];
+        keyUpTimes = new float[keyLength];
+        keyValues = new int[keyLength];
+        
+        keyDouble = new int[keyLength];
+        keyDoubleUp = new int[keyLength];
+        keyDoubleStay = new int[keyLength];
+
+        keyIndexs = new int[510];
+
+        for(int i=0; i<keyLength; i++) {
+            keyTimes[i] = 0;
+            keyUpTimes[i] = 0;
+            keyValues[i] = (int)System.Enum.GetValues(typeof(KeyCode)).GetValue(i);
+            keyDouble[i] = 0;
+            keyDoubleUp[i] = 0;
+            keyDoubleStay[i] = 0;
+
+            keyIndexs[keyValues[i]] = i;
+        }
+    }
+
+    void LateUpdate()
+    {
+        for(int i=0; i<keyLength; i++) {
+            if(Input.GetKey((KeyCode)keyValues[i])) {
+                keyTimes[i] += Time.deltaTime;
+                keyUpTimes[i] = 0;
+            }
+            else {
+                keyTimes[i] = 0f;
+                if(keyUpTimes[i] < limitTime)
+                    keyUpTimes[i] += Time.deltaTime;
+            }
+        }
+    }
+
+    public int Index(KeyCode key) {
+        return keyIndexs[(int)key];
+    }
+    public bool Stay(KeyCode key) {
+        return Input.GetKey(key);
+    }
+    public bool Down(KeyCode key) {
+        return Input.GetKeyDown(key);
+    }
+    public bool Up(KeyCode key) {
+        return Input.GetKeyUp(key);
+    }
+    public bool Long(KeyCode key, float time) {
+        return keyTimes[Index(key)] >= time;
+    }
+    public bool LongUp(KeyCode key, float time) {
+        return Input.GetKeyUp(key) && (keyTimes[Index(key)] >= time);
+    }
+    public bool Double(KeyCode key, float time) {
+        bool result = false;
+        if(keyTimes[Index(key)] >= time || keyUpTimes[Index(key)] >= time)
+            keyDouble[Index(key)] = 0;
+
+        if(Input.GetKeyDown(key))
+            keyDouble[Index(key)] += 1;
+
+        if(keyDouble[Index(key)] >= 2) {
+            result = true;
+            keyDouble[Index(key)] = 0;
+        }
+
+        return result;
+    }
+    public bool DoubleUp(KeyCode key, float time) {
+        bool result = false;
+        if(keyTimes[Index(key)] >= time || keyUpTimes[Index(key)] >= time)
+            keyDoubleUp[Index(key)] = 0;
+
+        if(Input.GetKeyUp(key))
+            keyDoubleUp[Index(key)] += 1;
+
+        if(keyDoubleUp[Index(key)] >= 2) {
+            result = true;
+            keyDoubleUp[Index(key)] = 0;
+        }
+
+        return result;
+    }
+    public bool DoubleStay(KeyCode key, float time) {
+        bool result = false;
+        if(keyTimes[Index(key)] >= time || keyUpTimes[Index(key)] >= time)
+            keyDoubleStay[Index(key)] = 0;
+        
+        if(Input.GetKeyUp(key) && keyDoubleStay[Index(key)] == 0)
+            keyDoubleStay[Index(key)] += 1;
+
+        if(keyDoubleUp[Index(key)] >= 2) {
+            result = true;
+            keyDoubleStay[Index(key)] = 0;
+        }
+
+        return result;
+    }
+}
---
+++ GroupGame/Assets/Resources/DongHyuk/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{   
+    CapsuleCollider coll;
+    Rigidbody rigid;
+    Vector3 velocity;
+    float rotationY;
+
+    bool isGround = false;
+    float groundCheckDistance = 0.1f;
+
+    float speed = 10;
+    float walkSpeed = 10;
+    float runSpeed = 20;
+
+    float jumpForce = 200;
+    bool isJump = false;
+    
+    void Awake() {
+        coll = GetComponent<CapsuleCollider>();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        speed = walkSpeed;
+    }
+
+    void Update()
+    {
+        /*if(KeyController.I.Long(KeyCode.Space, 1f))
+            Debug.Log("SPACE");
+        if(KeyController.I.LongUp(KeyCode.Space, 1f))
+            Debug.Log("SPACE UP");
+        if(KeyController.I.Double(KeyCode.F, 0.3f))
+            Debug.Log("F");
+        if(KeyController.I.Double(KeyCode.G, 0.3f))
+            Debug.Log("G");
+        if(KeyController.I.DoubleUp(KeyCode.H, 0.3f))
+            Debug.Log("H");*/
+
+        CheckKey();
+        Rotate();
+
+        rigid.MovePosition(transform.position + velocity * Time.deltaTime);
+    }
+
+    void FixedUpdate() {
+        CheckIsGround();
+        CheckJump();
+    }
+
+    void CheckIsGround() {
+        isGround = Physics.Raycast(transform.position, Vector3.down, coll.bounds.extents.y + groundCheckDistance);
+    }
+
+    void CheckJump() {
+        if(isJump) {
+            rigid.AddForce(Vector3.up * jumpForce);
+            isJump = false;
+        }
+    }
+    
+    void Rotate() {
+        transform.Rotate(0, rotationY, 0);
+    }
+
+    void CheckKey() {
+        CheckMoveKey();
+        CheckRunKey();
+        CheckRotateKey();
+        CheckJumpKey();
+    }
+
+    void CheckMoveKey() {
+        int _dash = 1;
+        float _horizontal = 0f, _vertical = 0f;
+
+        if(Input.GetKey(KeyCode.A))
+            _horizontal = -1f;
+        if(Input.GetKey(KeyCode.D))
+            _horizontal = 1f;
+        if(Input.GetKey(KeyCode.W))
+            _vertical = 1;
+        if(Input.GetKey(KeyCode.S))
+            _vertical = -1;
+
+        if(KeyController.I.Double(KeyCode.A, 0.5f))
+            _dash = 50;
+        if(KeyController.I.Double(KeyCode.D, 0.5f))
+            _dash = 50;
+
+
+        velocity = (transform.right * _horizontal) + (transform.forward * _vertical);
+        velocity = velocity.normalized * speed * _dash;
+    }
+
+    void CheckRunKey() {
+        if(Input.GetKey(KeyCode.LeftShift))
+            speed = runSpeed;
+        else
+            speed = walkSpeed;
+    }
+
+    void CheckRotateKey() {
+        rotationY = 0f;
+        if(Input.GetKey(KeyCode.LeftArrow))
+            rotationY = -1f;
+        if(Input.GetKey(KeyCode.RightArrow))
+            rotationY = 1f;
+    }
+
+    void CheckJumpKey() {
+        if(Input.GetKeyDown(KeyCode.Space) && isGround && !isJump)
+            isJump = true;
+    }
+}
